940bdff403e026508c40dce5cae8c64f29ddf236
Gaurav Singh <gaurav.singh323@gmail.com>
2022-08-18 11:29:04 +0530

    Change Ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useShop } from '../hooks'
 import style from '../styles/body.module.css'
 import Items from './smallComponents/Items'
+import shopImage from '../assets/images/shop.png'
 
 function Body() {
   const [items,setItems] = useState([]);
   const [title,setTitle] = useState('');
   const shop  = useShop();
-  // console.log(shop)
   useEffect(()=>{
     setItems(shop.currentItems);
     setTitle(shop.currentTitle);
@@ -21,6 +21,7 @@ function Body() {
           }) : ''}
           { title !== '' && items.length === 0 && <h5 style={{margin:20,color:'red'}}>No Items in this Category</h5>}
         </div>
+        { title === '' && <div><h1 style={{margin:20,color:'#000081',textAlign:'center'}}>Shop Your Favourite Items</h1><div style={{textAlign:'center'}}><img src={shopImage} alt='shop' width='650px'/></div></div>}
      
     </div>
   )
