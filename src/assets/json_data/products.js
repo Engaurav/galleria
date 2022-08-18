451a88a03973bd93fe76616bcbbb2714fa1055a7
Gaurav Singh <gaurav.singh323@gmail.com>
2022-08-18 12:42:46 +0530

    Added Some More Data in JSON

--- src/assets/json_data/products.js
+++ src/assets/json_data/products.js
@@ -1,177 +1,285 @@
 export const products = [{
-    id:"0",
-    title : "Mobile",
-    img : "https://i.ibb.co/cFMGqKk/3137807.png",
-    subcategory : [{
+  id:"0",
+  title : "Mobile",
+  img : "https://i.ibb.co/cFMGqKk/3137807.png",
+  subcategory : [{
+    sub_id : "0",
+    sub_title : "Apple",
+    items : [{
+      id : "0",
       sub_id : "0",
-      sub_title : "Apple",
-      items : [{
-        id : "0",
+      i_id : "0",
+      brand: "Apple",
+      detail : "iPhone 12 (Purple, 256 GB)",
+      price : 80900,
+      img: 'https://rukminim1.flixcart.com/image/416/416/ko0d6kw0/mobile/6/h/y/iphone-12-mjnm3hn-a-apple-original-imag2k2v6ehvnzfd.jpeg',
+      qty:0
+    },{
+      id : "0",
+      sub_id : "0",
+      i_id : "1",
+      brand: "Apple",
+      detail : "iPhone 11 (Purple, 64 GB)",
+      price : 49999,
+      qty:0,
+      img: 'https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/y/k/z/iphone-11-64-a-mwlx2hn-a-apple-0-original-imafkg24ymsjav9h.jpeg'
+    },{
+      id : "0",
+      sub_id : "0",
+      i_id : "2",
+      brand: "Apple",
+      detail : "iPhone 12 (Blue, 64 GB)",
+      price : 65999,
+      qty:0,
+      img: 'https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/y/7/n/apple-iphone-12-dummyapplefsn-original-imafwg8dpyjvgg3j.jpeg'
+    },]
+  },{
+    sub_id : "1",
+    sub_title : "Samsung",
+    items : [{
+      id : "0",
+      sub_id : "1",
+      i_id : "0",
+      brand: "Samsung",
+      detail : "Galaxy F12 (Sky Blue, 128 GB)  (4 GB RAM)",
+      price : 80900,
+      img: 'https://rukminim1.flixcart.com/image/416/416/kn22m4w0/mobile/9/k/s/galaxy-f12-sm-f127glbiins-samsung-original-imagftmhhhvghq7w.jpeg',
+      qty:0
+    },]
+  },{
+    sub_id : "2",
+    sub_title : "Oneplus",
+    items : [{
+      id : "0",
+      sub_id : "2",
+      i_id : "0",
+      brand: "OnePlus",
+      detail : "Nord CE 2 Lite 5G (Blue Tide, 128 GB)  (6 GB RAM)",
+      price : 80900,
+      img: 'https://rukminim1.flixcart.com/image/416/416/l2krs7k0/mobile/q/7/8/nord-ce-2-lite-5g-le2505-oneplus-original-imagdwfgyrzhgsnt.jpeg?',
+      qty:0
+    },]
+  },{
+    sub_id : "3",
+    sub_title : "Realme",
+    items : [{
+      id : "0",
+      sub_id : "3",
+      i_id : "0",
+      brand: "Realme",
+      detail : "C25_Y (Metal Grey, 128 GB)  (4 GB RAM)",
+      price : 80900,
+      img: 'https://rukminim1.flixcart.com/image/416/416/kwgpz0w0/mobile/r/6/w/-original-imag95yfhejsz5vp.jpeg',
+      qty:0
+    },]
+  },{
+    sub_id : "4",
+    sub_title : "Vivo",
+  },{
+    sub_id : "5",
+    sub_title : "Oppo",
+  },]
+},{
+  id : "1",
+  title : "Fashion",
+  img : "https://cdn-icons-png.flaticon.com/512/7417/7417663.png",
+  subcategory : [{
+    sub_id : "0",
+    sub_title : "Men's Top Wear",
+    further_category : [{
+      f_id : "0",
+      f_title : "Men's Tshirt",
+      items : [{  
+        id : "1",
         sub_id : "0",
+        f_id : "0",
         i_id : "0",
-        brand: "Apple",
-        detail : "iPhone 12 (Purple, 256 GB)",
-        price : 80900,
-        img: 'https://rukminim1.flixcart.com/image/416/416/ko0d6kw0/mobile/6/h/y/iphone-12-mjnm3hn-a-apple-original-imag2k2v6ehvnzfd.jpeg',
-        qty:0
+        brand: "Puma",
+        detail : "Graphic Print Men Round Neck White T-Shirt",
+        price : 1799,
+        qty:0,
+        img: 'https://rukminim1.flixcart.com/image/880/1056/l4hcx3k0/t-shirt/m/p/q/4xl-53681602-puma-original-imagfd76fettrqtm.jpeg'
       },{
-        id : "0",
+        id : "1",
         sub_id : "0",
+        f_id : "0",
         i_id : "1",
-        brand: "Apple",
-        detail : "iPhone 11 (Purple, 64 GB)",
-        price : 49999,
+        brand: "Nike",
+        detail : "Printed Men Round Neck Green T-Shirt",
+        price : 1595,
         qty:0,
-        img: 'https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/y/k/z/iphone-11-64-a-mwlx2hn-a-apple-0-original-imafkg24ymsjav9h.jpeg'
+        img: 'https://rukminim1.flixcart.com/image/880/1056/l41n2q80/t-shirt/4/q/g/m-dr1288-353-nike-original-imagff7uwnxttytv.jpeg'
       },{
-        id : "0",
+        id : "1",
         sub_id : "0",
+        f_id : "0",
         i_id : "2",
-        brand: "Apple",
-        detail : "iPhone 12 (Blue, 64 GB)",
-        price : 65999,
+        brand: "Flying Machine",
+        detail : "Solid Men Round Neck Black T-Shirt",
+        price : 999,
         qty:0,
-        img: 'https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/y/7/n/apple-iphone-12-dummyapplefsn-original-imafwg8dpyjvgg3j.jpeg'
+        img: 'https://rukminim1.flixcart.com/image/880/1056/l111lzk0/t-shirt/w/4/g/4xl-fmtsh0643-flying-machine-original-imagczenqxu9kcze.jpeg'
+      },{
+        id : "1",
+        sub_id : "0",
+        f_id : "0",
+        i_id : "3",
+        brand: "Flying Machine",
+        detail : "Printed Men Polo Neck Blue T-Shirt",
+        price : 1599,
+        qty:0,
+        img: 'https://rukminim1.flixcart.com/image/880/1056/kdga1zk0/t-shirt/t/p/0/xl-fmts3170-flying-machine-original-imafucx8zqymhj4m.jpeg'
+      },{
+        id : "1",
+        sub_id : "0",
+        f_id : "0",
+        i_id : "4",
+        brand: "Adidas",
+        detail : "Printed Men Round Neck White T-Shirt",
+        price : 1399,
+        qty:0,
+        img: 'https://rukminim1.flixcart.com/image/880/1056/xif0q/t-shirt/9/4/j/m-hk9160-adidas-original-imagh3zf5ehpzchg.jpeg'
       },]
     },{
-      sub_id : "1",
-      sub_title : "Samsung",
+      f_id : "1",
+      f_title : "Men's Casual Shirt"
     },{
-      sub_id : "2",
-      sub_title : "Oneplus",
+      f_id : "2",
+      f_title : "Men's Formal Shirt"
     },{
-      sub_id : "3",
-      sub_title : "Realme",
+      f_id : "3",
+      f_title : "Men's Blazer"
     },{
-      sub_id : "4",
-      sub_title : "Vivo",
-    },{
-      sub_id : "5",
-      sub_title : "Oppo",
+      f_id : "4",
+      f_title : "Men's Kurta"
     },]
   },{
-    id : "1",
-    title : "Fashion",
-    img : "https://cdn-icons-png.flaticon.com/512/7417/7417663.png",
-    subcategory : [{
-      sub_id : "0",
-      sub_title : "Men's Top Wear",
-      further_category : [{
-        f_id : "0",
-        f_title : "Men's Tshirt",
-        items : [{  
-          id : "1",
-          sub_id : "0",
-          f_id : "0",
-          i_id : "0",
-          brand: "Puma",
-          detail : "Graphic Print Men Round Neck White T-Shirt",
-          price : 1799,
-          qty:0,
-          img: 'https://rukminim1.flixcart.com/image/880/1056/l4hcx3k0/t-shirt/m/p/q/4xl-53681602-puma-original-imagfd76fettrqtm.jpeg'
-        },{
-          id : "1",
-          sub_id : "0",
-          f_id : "0",
-          i_id : "1",
-          brand: "Nike",
-          detail : "Printed Men Round Neck Green T-Shirt",
-          price : 1595,
-          qty:0,
-          img: 'https://rukminim1.flixcart.com/image/880/1056/l41n2q80/t-shirt/4/q/g/m-dr1288-353-nike-original-imagff7uwnxttytv.jpeg'
-        },{
-          id : "1",
-          sub_id : "0",
-          f_id : "0",
-          i_id : "2",
-          brand: "Flying Machine",
-          detail : "Solid Men Round Neck Black T-Shirt",
-          price : 999,
-          qty:0,
-          img: 'https://rukminim1.flixcart.com/image/880/1056/l111lzk0/t-shirt/w/4/g/4xl-fmtsh0643-flying-machine-original-imagczenqxu9kcze.jpeg'
-        },{
-          id : "1",
-          sub_id : "0",
-          f_id : "0",
-          i_id : "3",
-          brand: "Flying Machine",
-          detail : "Printed Men Polo Neck Blue T-Shirt",
-          price : 1599,
-          qty:0,
-          img: 'https://rukminim1.flixcart.com/image/880/1056/kdga1zk0/t-shirt/t/p/0/xl-fmts3170-flying-machine-original-imafucx8zqymhj4m.jpeg'
-        },{
-          id : "1",
-          sub_id : "0",
-          f_id : "0",
-          i_id : "4",
-          brand: "Adidas",
-          detail : "Printed Men Round Neck White T-Shirt",
-          price : 1399,
-          qty:0,
-          img: 'https://rukminim1.flixcart.com/image/880/1056/xif0q/t-shirt/9/4/j/m-hk9160-adidas-original-imagh3zf5ehpzchg.jpeg'
-        },]
-      },{
-        f_id : "1",
-        f_title : "Men's Casual Shirt"
-      },{
-        f_id : "2",
-        f_title : "Men's Formal Shirt"
-      },{
-        f_id : "3",
-        f_title : "Men's Blazer"
-      },{
-        f_id : "4",
-        f_title : "Men's Kurta"
-      },]
+    sub_id : "1",
+    sub_title : "Men's Bottom Wear",
+    further_category : [{
+      f_id : "0",
+      f_title : "Men's Jeans"
     },{
-      sub_id : "1",
-      sub_title : "Men's Bottom Wear",
-      further_category : [{
-        f_id : "0",
-        f_title : "Men's Jeans"
-      },{
-        f_id : "1",
-        f_title : "Men's Trouser"
-      },{
-        f_id : "2",
-        f_title : "Men's Formal Pant"
-      },{
-        f_id : "3",
-        f_title : "Men's Short"
-      },{
-        f_id : "4",
-        f_title : "Men's Cargos"
-      },]
+      f_id : "1",
+      f_title : "Men's Trouser"
     },{
-      sub_id : "2",
-      sub_title : "Women Ethnic",
+      f_id : "2",
+      f_title : "Men's Formal Pant"
     },{
-      sub_id : "3",
-      sub_title : "Women Western",
+      f_id : "3",
+      f_title : "Men's Short"
     },{
-      sub_id : "4",
-      sub_title : "Men Footwear",
+      f_id : "4",
+      f_title : "Men's Cargos"
+    },]
+  },{
+    sub_id : "2",
+    sub_title : "Women Ethnic",
+    further_category : [{
+      f_id : "0",
+      f_title : "Women Saree"
+    },{
+      f_id : "1",
+      f_title : "Women Ethinic"
+    },{
+      f_id : "2",
+      f_title : "Women Kurta"
+    },{
+      f_id : "3",
+      f_title : "Women Gows"
+    }]
+  },{
+    sub_id : "3",
+    sub_title : "Women Western",
+    further_category : [{
+      f_id : "0",
+      f_title : "Women Top"
+    },{
+      f_id : "1",
+      f_title : "Women Dress"
+    },{
+      f_id : "2",
+      f_title : "Women Jeans"
+    },{
+      f_id : "3",
+      f_title : "Women Tshirt"
+    }]
+  },{
+    sub_id : "4",
+    sub_title : "Men Footwear",
+    further_category : [{
+      f_id : "0",
+      f_title : "Sport"
+    },{
+      f_id : "1",
+      f_title : "Formal"
+    },{
+      f_id : "2",
+      f_title : "Slipper"
+    },{
+      f_id : "3",
+      f_title : "Casual"
+    }]
 
-    },{
-      sub_id : "5",
-      sub_title : "Women Footwear",
-    },{
-      sub_id : "6",
-      sub_title : "Watches and Accesories",
-    },{
-      sub_id : "7",
-      sub_title : "Bag's and Luggages",
-    },]
   },{
-    id:"2",
-    title : "Electronics",
-    img : 'https://i.ibb.co/QPXBG00/3659898.png'
+    sub_id : "5",
+    sub_title : "Women Footwear",
+    further_category : [{
+      f_id : "0",
+      f_title : "Sport"
+    },{
+      f_id : "1",
+      f_title : "Formal"
+    },{
+      f_id : "2",
+      f_title : "Slipper"
+    },{
+      f_id : "3",
+      f_title : "Casual"
+    }]
   },{
-    id:"3",
-    title:"Home",
-    img : 'https://cdn-icons-png.flaticon.com/512/5234/5234853.png'
+    sub_id : "6",
+    sub_title : "Watches and Accesories",
+    further_category : [{
+      f_id : "0",
+      f_title : "Men Watch"
+    },{
+      f_id : "1",
+      f_title : "Woment Watch"
+    },{
+      f_id : "2",
+      f_title : "Jwellary"
+    },{
+      f_id : "3",
+      f_title : "Men Accesories"
+    }]
   },{
-    id:"4",
-    title:"Appliances",
-    img :'https://i.ibb.co/9ZpPJ2c/3362679.png'
-  }]
+    sub_id : "7",
+    sub_title : "Bag's and Luggages",
+    further_category : [{
+      f_id : "0",
+      f_title : "School"
+    },{
+      f_id : "1",
+      f_title : "Rucksack"
+    },{
+      f_id : "2",
+      f_title : "Laptop"
+    },{
+      f_id : "3",
+      f_title : "HandBag"
+    }]
+  },]
+},{
+  id:"2",
+  title : "Electronics",
+  img : 'https://i.ibb.co/QPXBG00/3659898.png'
+},{
+  id:"3",
+  title:"Home",
+  img : 'https://cdn-icons-png.flaticon.com/512/5234/5234853.png'
+},{
+  id:"4",
+  title:"Appliances",
+  img :'https://i.ibb.co/9ZpPJ2c/3362679.png'
+}]
